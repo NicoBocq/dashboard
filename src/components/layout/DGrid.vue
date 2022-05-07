77aa1316ffb6c7d15c9a59350182bf0c802235a2
Nicolas Bocquet <macbocq@MacBook-Pro-de-Nicolas.local>
2022-05-07 16:52:31 +0200

    Init ui

---
+++ src/components/layout/DGrid.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ src/components/layout/DSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <section aria-labelledby="products-heading" class="mt-8">
+    <h2 id="products-heading" class="sr-only">
+      {{ title }}
+    </h2>
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+interface DSectionProps {
+  title: string
+}
+
+const props = defineProps<DSectionProps>()
+</script>
